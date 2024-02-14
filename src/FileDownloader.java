ae17c67441f658f6e188414a79b7d54176913611
Konrad Micek <98konrad98@gmail.com>
2024-02-14 19:58:31 +0100

    Init downloading

--- src/FileDownloader.java
+++ src/FileDownloader.java
@@ -1,22 +1,39 @@
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class FileDownloader {
-    private final HttpURLConnection httpConnection;
-    private final long fileSize;
-    private final long blockSize;
+    private final int blockSize;
+    private final String fileName;
+    private final BufferedInputStream input;
 
-    public FileDownloader(String _url, long _blockSize) throws IOException {
+    public FileDownloader(String _url, int _blockSize, String _fileName) throws IOException {
         blockSize = _blockSize;
+        fileName = _fileName;
 
-        URL url = new URL(_url);
-        httpConnection = (HttpURLConnection) url.openConnection();
-        httpConnection.setRequestMethod("HEAD");
-        fileSize = httpConnection.getContentLengthLong();
+        input = new BufferedInputStream(new URL(_url).openStream());
     }
 
-    public void downloadBlock() {
+    public boolean downloadBlock(int blockNumber) throws IOException {
+        String partFileName = fileName + "." + blockNumber;
+        int bytesRead;
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(partFileName)) {
+            int startByte = blockNumber * blockSize;
+            byte[] dataBuffer = new byte[blockSize];
+
+            do {
+                bytesRead = input.read(dataBuffer, startByte, startByte + blockSize);
+            } while (bytesRead != -1 && bytesRead != 0 && bytesRead != blockSize);
+
+            fileOutputStream.write(dataBuffer, 0, bytesRead);
+        }
+
+        return bytesRead != blockSize;
+    }
+
+    public void joinParts() {
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        String url = "https://raw.githubusercontent.com/dscape/spell/master/test/resources/big.txt";
+        int blockSize = 2222;
+        String fileName = "test";
+
+        FileDownloader downloader = new FileDownloader(url, blockSize, fileName);
+        downloader.downloadBlock(0);
+        downloader.downloadBlock(1);
     }
 }
