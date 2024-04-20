a5b33f4036a0a244372b1100bac6cb68769c0fc4
Konrad Micek <98konrad98@gmail.com>
2024-04-20 14:31:32 +0200

    Repaired test

--- src/server/udp/SendCommandCallback.java
+++ src/server/udp/SendCommandCallback.java
@@ -1,7 +1,5 @@
 package server.udp;
 
-import common.command.Command;
-
 import java.nio.file.Path;
 
 public interface SendCommandCallback {
--- test/DownloaderTests.java
+++ test/DownloaderTests.java
@@ -1,8 +1,11 @@
 import common.DownloadException;
+import common.udp.FileInfoHolder;
+import common.utils.FilePartUtils;
 import common.utils.PrepareDownloadUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import server.FileDownloader;
+import server.udp.SendCommandCallback;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,36 +16,42 @@ import java.nio.file.Paths;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class DownloaderTests {
     @Test
     public void testOriginalAndDownloadedFilesChecksums() throws IOException, NoSuchAlgorithmException, DownloadException {
-        String name = "testOriginalAndDownloadedFilesChecksums";
-        String destinationDirectoryName = String.valueOf(PrepareDownloadUtils.serverDownloadPath);
-        Path testFilePath = Paths.get(destinationDirectoryName, name);
+        PrepareDownloadUtils.initProgram();
+
+        FileInfoHolder fileInfoHolder = new FileInfoHolder();
+        SendCommandCallback callback = filePartPath -> {
+        };
+
+        String testDirectory = "filesTest";
+        Files.createDirectories(Paths.get(testDirectory));
+        String fileName= "testOriginalAndDownloadedFilesChecksums.file";
+        Path testFilePath = Paths.get(testDirectory, fileName);
         URL url = testFilePath.toUri().toURL();
-        FileDownloader fileDownloader = new FileDownloader(url.toString(), 1);
+        FileDownloader fileDownloader = new FileDownloader(url.toString(), 1, fileInfoHolder, callback);
 
         byte[] bytes = new byte[1024 * 1024 * 10];
         new Random(22).nextBytes(bytes);
         try {
             Files.write(testFilePath, bytes);
 
-            fileDownloader.downloadWholeFile();
-            Assertions.assertTrue(fileDownloader.joinDeleteFileParts());
+            int result = fileDownloader.call();
+            Assertions.assertEquals(0, result);
+            Assertions.assertTrue(FilePartUtils.joinAndDeleteFileParts(new ArrayList<>(fileInfoHolder.filesToProcess)));
 
             byte[] originalChecksum = fileChecksum(testFilePath);
             byte[] downloadedChecksum = fileChecksum(PrepareDownloadUtils.serverDownloadPath);
 
             Assertions.assertArrayEquals(originalChecksum, downloadedChecksum);
         } finally {
-            Files.deleteIfExists(testFilePath);
-            for (int i = 0; i < 11; i++) {
-                Path path = Paths.get(destinationDirectoryName, name + ".part" + i);
-                Files.deleteIfExists(path);
-            }
-            Files.deleteIfExists(testFilePath);
+            FilePartUtils.removeFileParts(new ArrayList<>(fileInfoHolder.filesToProcess));
+            FilePartUtils.removeFileParts(Collections.singletonList(testFilePath));
         }
     }
 
